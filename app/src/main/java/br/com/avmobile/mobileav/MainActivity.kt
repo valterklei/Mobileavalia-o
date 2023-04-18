1a50cb8d428516e0f0a27b9e7148f19c0cd20a2c
valte <valterklei@gmail.com>
2023-04-18 11:03:46 -0300

    prova de mobile

---
+++ app/src/main/java/br/com/avmobile/mobileav/MainActivity.kt
@@ -0,0 +1,16 @@
+package br.com.avmobile.mobileav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+
+
+
+}
---
+++ app/src/main/java/br/com/avmobile/mobileav/paginainicial.kt
@@ -0,0 +1,11 @@
+package br.com.avmobile.mobileav
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class paginainicial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_paginainicial)
+    }
+}
